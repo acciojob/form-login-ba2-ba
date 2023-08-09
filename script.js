d3956001499d146bcc67edaa03369eac35fe763e
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-08-09 23:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function getFormvalue() {
     //Write your code here
-
-	 
-function getFormvalue() {
-    //Write your code here
 let first =document.querySelector('#form1 input[name="fname"]');
 let last =document.querySelector('#form1 input[name="lname"]');
 	let fname=first.value;
